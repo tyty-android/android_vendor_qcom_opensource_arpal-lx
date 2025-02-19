0c00dd33fa0d23ea3c8603bf73aca1c1f8cf94bc
Huang, Yidong <quic_yidongh@quicinc.com>
2025-02-19 17:02:58 +0800

    pal: avoid improper device registration during device switch
    When switching device for Rx stream in INIT state, new device is
    registered unexpectedly, causing EC not enabled properly if Rx
    device metadata is not updated to AGM yet.
    
    Add check to avoid registering new device when switching for
    stream in INIT state.
    
    Change-Id: I16cce2335d57594aa0560f0c3e974309917cfb9b

--- stream/src/Stream.cpp
+++ stream/src/Stream.cpp
@@ -1134,7 +1134,7 @@ int32_t Stream::disconnectStreamDevice_l(Stream* streamHandle, pal_device_id_t d
         if (dev_id == mDevices[i]->getSndDeviceId()) {
             PAL_DBG(LOG_TAG, "device %d name %s, going to stop",
                 mDevices[i]->getSndDeviceId(), mDevices[i]->getPALDeviceName().c_str());
-            if (currentState != STREAM_STOPPED) {
+            if (currentState != STREAM_INIT && currentState != STREAM_STOPPED) {
                 rm->deregisterDevice(mDevices[i], this);
             }
             rm->lockGraph();
@@ -1301,7 +1301,7 @@ int32_t Stream::connectStreamDevice_l(Stream* streamHandle, struct pal_device *d
         goto dev_stop;
     }
     rm->unlockGraph();
-    if (currentState != STREAM_STOPPED) {
+    if (currentState != STREAM_INIT && currentState != STREAM_STOPPED) {
         rm->registerDevice(dev, this);
     }
 
